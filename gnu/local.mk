06365318f0f9fb59b286ec03e63ac5888d76c46b
Ryan Sundberg <ryan@arctype.co>
2021-05-08 22:53:49 -0700

    python-keystoneauth1: new package

--- gnu/local.mk
+++ gnu/local.mk
@@ -1914,6 +1914,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-pillow-CVE-2022-45199.patch	\
   %D%/packages/patches/python-pyfakefs-remove-bad-test.patch	\
   %D%/packages/patches/python-flint-includes.patch		\
+  %D%/packages/patches/python-keystoneauth1-requirements.patch \
   %D%/packages/patches/python-libxml2-utf8.patch		\
   %D%/packages/patches/python-memcached-syntax-warnings.patch	\
   %D%/packages/patches/python-mox3-python3.6-compat.patch	\
