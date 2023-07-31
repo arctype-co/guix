0aa306d8ceab70272bade1b1ba6673e43b49b884
Ryan Sundberg <ryan@arctype.co>
2023-07-30 22:13:56 -0700

    arctype-guix: Re-release
    Change-Id: I5c430a86cc716cd61fadeef2a363af90063642ba

--- gnu/packages/package-management.scm
+++ gnu/packages/package-management.scm
@@ -573,16 +573,16 @@ the Nix package manager.")
   (package
     (inherit guix)
     (name "arctype-guix")
-    (version "e1c587e5f23fcf76616fccaee39910a941ffd5f3")
+    (version "f48cd25263d080b1ade9097a5dd7e8a207f77909")
     (source
       (origin
         (method git-fetch)
         (uri (git-reference
-               (url "https://lab.arctype.co/arctype/guix")
+               (url "https://github.com/arctype-co/guix")
                (commit version)))
         (sha256
           (base32
-            "1aslbzib0n269vi4y6a73wx6cnijqvrm9856ycm4754cnq9xjcwz"))
+            "1c3z81b92w3y70373q4zcnh20hh93cn60yv1a1sy28b78a859n93"))
         (file-name (string-append "arctype-guix-" version "-checkout"))))
     (arguments
      (substitute-keyword-arguments (package-arguments guix)
